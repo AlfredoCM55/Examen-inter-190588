f1eefb7a78fcc7dde257f750b1e172a9e4c73966
Joy.Macias <190588@utags.edu.mx>
2020-12-16 15:32:59 -0600

    Usuario y Empleados preparados

--- server/server.js
+++ server/server.js
@@ -1,73 +1,32 @@
-require('./config/config')
+require('./config/config');
 const express = require('express');
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
-
-app.use(bodyParser.json());
- 
+app.use(bodyParser.json())
 app.get('/', function (req, res) {
-  res.send('<h1>Bievenido a mi servidor Rest de Jose Alfredo</h1>');
+  res.send('<h1>Servidor rest Jose Alfredo</h1>')
 });
 
-app.get('/usuario', function (req, res) {
-    res.json({
-        ok:200,
-        mensaje: 'Usuarios consultados con exito'
+app.use(require('./Routes/usuario'));
+app.use(require('./Routes/departamento'));
+app.use(require('./Routes/empleado'));  
 
-    });
-  });
-
-  app.post('/usuario', function(req,res ){
-    let nombre = req.body.nombre; 
-    let body = req.body;
-
-    if(nombre === undefined){
-        res.status(400).json({
-            ok: 400,
-            mensaje: 'Favor de enviar el valor nombre'
-
-        });
-    }else{
-
+mongoose.connect('mongodb://localhost:27017/examen', {
     
-        res.json({
-          ok:200,
-          mensaje: 'Usuario insertado con exito',
-          body: body
-
-      });
-    }
-    
-  })
-
-  app.put('/usuario/:id/:nombre', function (req, res ){
-      let id = req.params.id;
-      let nombre = req.params.nombre;
-
-      res.json({
-          ok:200,
-          mensaje: 'Usuario actualizado con exito',
-          id: id,
-          nombre: nombre
-      });
-  });
-
-  app.delete('/usuario/:id', function (req, res){
-      let id = req.params.id;
-
-      res.json({
-          ok:200,
-          mensaje: 'Usuario eliminado con exito',
-          id: id
-      });
-
-  });
-
-app.listen(process.env.PORT, () => {
-    console.log('El servidor esta en linea en el puerto', process.env.PORT);
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+},(err, res) => {
+ if(err) throw err;
+ console.log('Base de datos ONLINE');
+});
+ 
+app.listen(process.env.PORT, () =>{
+    console.log('El servidor esta en linea por el puerto', process.env.PORT);
 });
